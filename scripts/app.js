4da22e7abc3f8b1d66832eac319ae9aade0b6f69
renato76 <renmignogna@gmail.com>
2020-08-27 20:34:34 +0100

    css added to grid, JS shows each unique grid location

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,14 @@ function init() {
   const width = 7 // how many cells wide is the grid
   const gridCellCount = width * 6 // how many cells we want
 
-  
+
 
   function createGrid() {
     for (let i = 0; i < gridCellCount; i++) { // this adds id number to each cell / div
       const cell = document.createElement('div')
       cell.setAttribute('id', i)
-      grid.appendChild(cell)
       cells.push(cell)
+      grid.appendChild(cell)
     }
     console.log('cells', cells)
   }
