279a5191fd53145ceb0d6042add87d077980f02a
renato76 <renmignogna@gmail.com>
2020-08-27 16:47:57 +0100

    added notes for the plan

--- scripts/plan.js
+++ scripts/plan.js
@@ -21,7 +21,9 @@ Styling for player 1 & player 2 pieces
 Styling for the scores and for the controls
 Frame for the grid
 
-Icons: Need 2 colored icons as pieces
+Icons: Need 2 colored icons as pieces. RED & YELLOW
+
+Need an indicator Icon like a white arrow or similar
 
 
 
@@ -31,7 +33,7 @@ Icons: Need 2 colored icons as pieces
 
 General Notes
 
-7 x 6 grid generated with a for loop
+// 7 x 6 grid generated with a for loop
 
 Player 1 Red
 Player 2 Yellow
@@ -52,17 +54,24 @@ Player 1 starts first, lets say RED
 
 *****************      GRID EVENTS      ******************
 
-indicator moves on on mouse hover or mouseenter, according to which column mouse is in
-on mouse out, so if going off the grid, then should just be normal mouse cursor
+1. indicator at top - moves on on mouse hover or mouseenter, according to which column mouse is in
+This will need a function that can see which grid column mouse is in, and copy that column for the indicator
+
+2. on mouse out, so if going off the grid, then should just be normal mouse cursor
+This will need some sort of switch off feature or similar
 
 what happens when....user clicks a column?
-When a user clicks a column, it should drop his piece into that column
+When a user clicks a column, it should drop his piece into that column, and you should see the piece move down smoothly and end bouncing.
 
 when a user hovers across, there should be an indicator at top to show which column he is in.
 
 It should not matter what height row they are in, that should make no difference, it's only about columns
 
 
+
+
+
+**************** Calculating The WIN LOGIC  ***************
 
 
 
