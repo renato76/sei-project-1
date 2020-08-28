134f709fdcb54a9dbc72f625dd96ac49c7a32aa0
renato76 <renmignogna@gmail.com>
2020-08-28 12:08:47 +0100

    added some js

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,10 @@ function init() {
   const width = 7 // how many cells wide is the grid
   const gridCellCount = width * 6 // how many cells we want
 
-
+  const player1 = []
+  const player2 = []
+  
+  // Function to create the playing grid
 
   function createGrid() {
     for (let i = 0; i < gridCellCount; i++) { // this adds id number to each cell / div
@@ -21,12 +24,43 @@ function init() {
 
   createGrid()
 
+  // Create function that can listen for mouse hovering 
+  // over board and return a column number
+  
+  cells.forEach(cell => {
+    cell.addEventListener('click', handleHover)
+  }) 
+  
+  function handleHover(e) {
+    const cell = e.target.id
+    console.log(cell % width)
+  }
+
+  cells.forEach(cell => {
+    cell.addEventListener('click', handleHover)
+  })
+
+
+  // Create a function that return row id
+
+  
+
+
+  // create a function that will change the bg color of the first empty cell
+  // in the chosen column
 
 
 
 
 
 
+
+  
+  
+
+
+
+  
 
 
 
